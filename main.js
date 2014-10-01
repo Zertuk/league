ea692bb091618c08eea5273a2ae693f2f57574ee
Justin <zertukis@gmail.com>
2014-09-30 21:31:59 -0500

    adding api call for getting the total creepscore in a game

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+var summonerName = "";
+var summonerId = "";
+var apiKey = "";
+
+
 
 function summonerLookUp() {
-	var summonerName = "";
-	var summonerId = "";
+	summonerName = "";
+	summonerId = "";
 
 	summonerName = $("#user_name").val();
 
-	var apiKey = '';
 
 	$.ajax({
 		url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + summonerName + '?api_key=' + apiKey,
@@ -19,11 +23,37 @@ function summonerLookUp() {
 			summonerNameMinusSpaces = summonerNameMinusSpaces.toLowerCase().trim();
 			summonerId = json[summonerNameMinusSpaces].id;
 			console.log(summonerId);
+			creepScoreLookUp();
 
 		},
 		error: function(XMLHttpRequest, testStatus, errorThrown) {
 			alert('error');
 		}
 	})
+}
+
+function creepScoreLookUp() {
+	var creepScore = "";
+
+	$.ajax({
+		url: 'https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/' + summonerId + '?api_key=' + apiKey,
+		type: 'GET',
+		dataType: 'json',
+		data: {
+
+		},
+
+		success: function(json) {
+			creepscore = json[summonerId].stats;
+			// creepScore = json['matches'][0].participants.stats.minionsKilled + json['matches'][0].participants.stats.neutralMinionsKilled;
+
+			console.log(creepScore);
+		},
+
+		error: function(XMLHttpRequest, testStatus, errorThrown) {
+			alert('error');
+		}
+	})
+
 
 }
