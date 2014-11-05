f1f95b6cdfb546cbe99de5db9a3ea978ba51285b
Justin <zertukis@gmail.com>
2014-11-05 01:31:20 -0600

    added new js files to input spell/champion id and return the name

---
+++ app/scripts/spellSelect.js
@@ -0,0 +1,17 @@
+function spellSelect(id) {
+	switch(parseInt(id)) {
+		default: return "N/A"
+		break;
+		case 6: return "Ghost"
+		break;
+		case 12: return "Teleport"
+		break;
+		case 14: return "Ignite"
+		break;
+		case 3: return "Exhaust"
+		break;
+		case 4: return "Flash"
+		break;
+
+	}
+}
