082fc0d4085b27d22c9dafc197c0c46448df0745
Justin <zertukis@gmail.com>
2014-11-05 01:33:12 -0600

    added images for champions and some summoners spells, now saves those images using new angular functions, spellIdNameMap and champIdNameMap and displays them, css changes to align new content nicely

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,11 @@
 var summonerId;
 var apiKey = '85df569c-14df-466c-966b-81f186ed0940';
 var gameInfo;
+var champNames = [];
+var champImages = [];
+var spellNames = [];
+var spellImages = [];
+
 
 angular.module('leagueApp')
   .controller('MainCtrl', function ($scope, $http) {
@@ -36,11 +41,33 @@ angular.module('leagueApp')
 		success (function(json) {
 			gameInfo = json.games;
 			$scope.gameInfo = gameInfo;
+			console.log($scope.gameInfo);
+			$scope.championIdNameMap(gameInfo);
+			$scope.spellIdNameMap(gameInfo);
 		}).
 		error (function() {
 			alert('not ok');
 		})
 	};
+
+	$scope.championIdNameMap = function(data) {
+		for (var i = 0; i < 10; i++) {
+        	champNames[i] = champSelect(data[i].championId);
+        	$scope.gameInfo[i].champNames = champNames[i];
+        	champImages[i] = '/images/champion/' + champNames[i] + '.png'
+        	$scope.gameInfo[i].champImages = champImages[i];
+        	console.log($scope.gameInfo[i].champImages);
+        }
+	};
+
+	$scope.spellIdNameMap = function(data) {
+		for (var i = 0; i < 10; i++) {
+			spellNames[i] = [spellSelect(data[i].spell1), spellSelect(data[i].spell2)];
+			$scope.gameInfo[i].spellNames = spellNames[i];
+			spellImages[i] = ['/images/spells/' + spellNames[i][0] + '.png' , '/images/spells/' + spellNames[i][1] + '.png']
+			$scope.gameInfo[i].spellImages = spellImages[i];
+		}
+	}
 
 
 
---
+++ main.js
@@ -0,0 +1,68 @@
+var summonerName = "";
+var summonerId = "";
+var apiKey = "85df569c-14df-466c-966b-81f186ed0940";
+
+
+
+function summonerLookUp() {
+	summonerName = "";
+	summonerId = "";
+
+	summonerName = $("#user_name").val();
+
+
+	$.ajax({
+		url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + summonerName + '?api_key=' + apiKey,
+		type: 'GET',
+		dataType: 'json',
+		data: {
+
+		},
+		success: function(json) {
+			var summonerNameMinusSpaces = summonerName.replace(" ", "");
+			summonerNameMinusSpaces = summonerNameMinusSpaces.toLowerCase().trim();
+			summonerId = json[summonerNameMinusSpaces].id;
+			console.log(summonerId);
+			creepScoreLookUp();
+
+		},
+		error: function(XMLHttpRequest, testStatus, errorThrown) {
+			alert('error');
+		}
+	})
+}
+var testage;
+function creepScoreLookUp() {
+	$.ajax({
+		url: 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/' + summonerId + '/recent' + '?api_key=' + apiKey,
+		type: 'GET',
+		dataType: 'json',
+		data: {
+
+		},
+
+		success: function(json) {
+			testage = json;
+			console.log('yay');
+		},
+
+		error: function(XMLHttpRequest, testStatus, errorThrown) {
+			alert('error');
+		}
+	})
+
+function championIdNameMap() {
+	$.ajax({
+		url: "https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/?api_key=85df569c-14df-466c-966b-81f186ed0940",
+		type: 'get',
+		dataType: 'json',
+		data: {
+
+		},
+		success: function
+	})
+}
+
+
+}
+
