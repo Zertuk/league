387fae65097962cce46bd0a28289adf3246ed8a9
Justin <zertukis@gmail.com>
2014-11-17 23:54:41 -0600

    added some comments and removed some old unneeded code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -45,7 +45,7 @@ angular.module('leagueApp')
 			})
 		};
 
-	//api call for match look up using summoner Id, also runs a
+	//api call for match look up using summoner Id, also runs a mass function call for all information extracted from gameInfo and runs other api calls
 	$scope.matchLookUp = function() {
 		$http.get('https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/' + summonerId + '/recent' + '?api_key=' + apiKey).
 		success (function(json) {
@@ -53,12 +53,13 @@ angular.module('leagueApp')
 			$scope.gameInfo = gameInfo;
 			$scope.massFunctionCall();
 			$('.gameSummary').show();
-
 		}).
 		error (function() {
 			alert('not ok');
 		})
 	};
+
+	//function run all other api calls and extract other game info
 	$scope.massFunctionCall = function() {
 			$scope.championIdNameMap(gameInfo);
 			$scope.spellIdNameMap(gameInfo);
@@ -69,20 +70,23 @@ angular.module('leagueApp')
 			$scope.leagueLookUp();
 	}
 
+	//api call for summoner champion stats by summoner id, also sets champion names
 	$scope.statLookUp = function() {
 		$http.get('https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/' + summonerId + '/ranked?season=SEASON4&api_key=' + apiKey).
 		success (function(json) {
 			statInfo = json;
-			var j = 0;
 			var i = 0;
 			while (statInfo.champions[i].id != undefined) {
+				//adds i value here so that if the next value is undefined we dip out so it doesnt break
 				i = i + 1;
 				if (statInfo.champions[i] === undefined) {
+					//adds 0 value since we skippde it earlier
 					statInfo.champions[0].champName = champSelect(statInfo.champions[0].id);
 					$scope.gameInfo.statInfo = statInfo;
 					return;
 				}
 				else {
+				//sets our champion name if value exists
 				statInfo.champions[i].champName = champSelect(statInfo.champions[i].id);
 				}
 			}
@@ -90,10 +94,11 @@ angular.module('leagueApp')
 
 		}).
 		error (function() {
-			alert('not ok');
+			alert('Champion Info not found');
 		})
 	}
 
+	//api call for league info using summoner Id
 	$scope.leagueLookUp = function() {
 		$http.get('https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/' + summonerId + '/entry?api_key=' + apiKey).
 		success (function(json) {
@@ -102,25 +107,23 @@ angular.module('leagueApp')
 
 		}).
 		error (function() {
-			alert('not ok');
+			alert('League Info not found');
 		})
 	}
 
+	//maps all champion id for summoners previous 10 games / assigns image location
 	$scope.championIdNameMap = function(data) {
 		for (var i = 0; i < 10; i++) {
-			// for (var j = 0; j < 10; j++) {
-   //      		teamChamps[i] = data[i].fellowPlayers[j].championId;
-   //      	}
         	champNames[i] = champSelect(data[i].championId);
         	$scope.gameInfo[i].champNames = champNames[i];
         	champImages[i] = '/images/champion/' + champNames[i] + '.png'
         	$scope.gameInfo[i].champImages = champImages[i];
         	console.log($scope.gameInfo[i].champImages);
-	       	$scope.gameInfo[i].toolTip = '<div class = "itemToolTip"><p class = "itemName">{{itemInfo.name}}</p><p class = "itemDescription" ng-bind-html = "itemInfo.description"></p><p class = "itemText">{{itemInfo.plaintext}}</p></div>';
        	       	
         }
 	};
 
+	//summoner spell map using spellSelect function, assigns image location
 	$scope.spellIdNameMap = function(data) {
 		for (var i = 0; i < 10; i++) {
 			spellNames[i] = [spellSelect(data[i].spell1), spellSelect(data[i].spell2)];
@@ -130,34 +133,43 @@ angular.module('leagueApp')
 			$scope.gameInfo[i].itemName = "/images/item/" + data[i].stats.item0 + ".png";
 		}
 	}
+
+	//maps champion names for all teammates in each game
+	//j controls the match index
+	//i controls the player index inside of each match
+	//k represents the position that the player is added at on their respective team
 	$scope.teamChampMap = function(data) {
 		for (var j = 0; j < 10; j++) {
+			//this is the player case, it is not included in the fellowPlayers so we add it here
 			teamChamps[0] = champSelect(data[j].championId);
+
 			var k = 1;
 			var l = 0;
 			for (var i = 0; i < 9; i++) {
+				//if there is only 1 player in a custom game, fellowPlayers doesnt exist
+				//so we check to make sure it does to prevent error
 				if (data[j].fellowPlayers) {
-				if (data[j].fellowPlayers[i] == undefined) {
-        			console.log('done')
-        		}
-        		else {
-					if (data[j].fellowPlayers[i].teamId == data[j].teamId) {
-						teamChamps[k] = champSelect(data[j].fellowPlayers[i].championId);
-						k++;
+					//checking this makes sure we dont check an index that doesnt exist (if there are less than 9 teammates in a game)
+					if (data[j].fellowPlayers[i] != undefined) {
+						//sorts to each team
+						if (data[j].fellowPlayers[i].teamId == data[j].teamId) {
+							teamChamps[k] = champSelect(data[j].fellowPlayers[i].championId);
+							k++;
+						}
+						else {
+							enemyChamps[l] = champSelect(data[j].fellowPlayers[i].championId);
+							l++;
+						}
 					}
-					else {
-						enemyChamps[l] = champSelect(data[j].fellowPlayers[i].championId);
-						l++;
-					}
+					data[j].fellowPlayers.teamChamps = teamChamps;
+					data[j].fellowPlayers.enemyChamps = enemyChamps;
 				}
-				data[j].fellowPlayers.teamChamps = teamChamps;
-				data[j].fellowPlayers.enemyChamps = enemyChamps;
 			}
-			}
-			enemyChamps = [];
+			enemyChamps = []; 
 			teamChamps = [];
 		}
 	}
+	//assigns color based on win/loss result
 	$scope.teamColor = function(data) {
 		$scope.gameInfo.lose = 0;
 		$scope.gameInfo.win = 0;
@@ -174,6 +186,7 @@ angular.module('leagueApp')
 			$scope.gameInfo[i].color = teamColor[i];
 		}
 	}
+	//goes through each item in each game to get rid of the undefined values and set to 'empty' which provides a 1x1 transparent image
 	$scope.itemFix = function(data) {
 		for (var i = 0; i < 10; i++) {
 			if (data[i].stats.item0 == undefined) {
@@ -219,8 +232,9 @@ angular.module('leagueApp')
 		// $scope.itemInfo = itemInfo;
 		console.log('OK');
 	}
+	//default search!
 	$scope.defaultSearch = function() {
-		$scope.summonerName = 'Zertuk';
+		$scope.summonerName = 'Dyrus';
 		$scope.apiCall();
 	}
 	$scope.defaultSearch();
