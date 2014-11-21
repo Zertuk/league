7b35001adb20b06520bb2561a41d102473874289
Justin <zertukis@gmail.com>
2014-11-20 21:39:35 -0600

    commented out the item hover functions since they arent being used currently

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,16 +16,16 @@ var spellNames = [];
 var spellImages = [];
 var teamColor = [];
 var teamChamps = [];
-var teamNames = [];
 var enemyChamps = [];
 var itemInfo = [];
-var testingHere;
 var statInfo = [];
 var leagueInfo = [];
 var killAverage;
 var csAverage;
 var assistAverage;
 var deathAverage;
+var test;
+var arr = new Array;
 
 
 angular.module('leagueApp')
@@ -33,10 +33,10 @@ angular.module('leagueApp')
   	//api call for the summoner id by name, changes summoner name to not include spaces to prevent errors
   	//runs matchLookUp api call to continue
   	$scope.apiCall = function() {
-		console.log($scope.summonerName)
+		console.log($scope.summonerName);
 		$http.get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + $scope.summonerName + '?api_key=' + apiKey).
 		success (function(json) {
-			var summonerNameMinusSpaces = $scope.summonerName.replace(" ", "");
+			var summonerNameMinusSpaces = $scope.summonerName.replace(' ', '');
 			summonerNameMinusSpaces = summonerNameMinusSpaces.toLowerCase().trim();
 			summonerId = json[summonerNameMinusSpaces].id;
 			console.log(summonerId);
@@ -46,7 +46,7 @@ angular.module('leagueApp')
 		}).
 		error (function() {
 			alert('Summoner Name not Found');
-			})
+			});
 		};
 
 	//api call for match look up using summoner Id, also runs a mass function call for all information extracted from gameInfo and runs other api calls
@@ -60,7 +60,7 @@ angular.module('leagueApp')
 		}).
 		error (function() {
 			alert('not ok');
-		})
+		});
 	};
 
 	//function run all other api calls and extract other game info
@@ -73,7 +73,7 @@ angular.module('leagueApp')
 			$scope.statLookUp();
 			$scope.leagueLookUp();
 			$scope.gameAverages(gameInfo);
-	}
+	};
 
 	//api call for summoner champion stats by summoner id, also sets champion names
 	$scope.statLookUp = function() {
@@ -81,7 +81,7 @@ angular.module('leagueApp')
 		success (function(json) {
 			statInfo = json;
 			var i = 0;
-			while (statInfo.champions[i].id != undefined) {
+			while (statInfo.champions[i].id !== undefined) {
 				//adds i value here so that if the next value is undefined we dip out so it doesnt break
 				i = i + 1;
 				if (statInfo.champions[i] === undefined) {
@@ -100,8 +100,8 @@ angular.module('leagueApp')
 		}).
 		error (function() {
 			alert('Champion Info not found');
-		})
-	}
+		});
+	};
 
 	//api call for league info using summoner Id
 	$scope.leagueLookUp = function() {
@@ -113,15 +113,15 @@ angular.module('leagueApp')
 		}).
 		error (function() {
 			alert('League Info not found');
-		})
-	}
+		});
+	};
 
 	//maps all champion id for summoners previous 10 games / assigns image location
 	$scope.championIdNameMap = function(data) {
 		for (var i = 0; i < 10; i++) {
         	champNames[i] = champSelect(data[i].championId);
         	$scope.gameInfo[i].champNames = champNames[i];
-        	champImages[i] = '/images/champion/' + champNames[i] + '.png'
+        	champImages[i] = '/images/champion/' + champNames[i] + '.png';
         	$scope.gameInfo[i].champImages = champImages[i];
         	console.log($scope.gameInfo[i].champImages);
        	       	
@@ -133,11 +133,11 @@ angular.module('leagueApp')
 		for (var i = 0; i < 10; i++) {
 			spellNames[i] = [spellSelect(data[i].spell1), spellSelect(data[i].spell2)];
 			$scope.gameInfo[i].spellNames = spellNames[i];
-			spellImages[i] = ['/images/spells/' + spellNames[i][0] + '.png' , '/images/spells/' + spellNames[i][1] + '.png']
+			spellImages[i] = ['/images/spells/' + spellNames[i][0] + '.png' , '/images/spells/' + spellNames[i][1] + '.png'];
 			$scope.gameInfo[i].spellImages = spellImages[i];
-			$scope.gameInfo[i].itemName = "/images/item/" + data[i].stats.item0 + ".png";
+			$scope.gameInfo[i].itemName = '/images/item/' + data[i].stats.item0 + '.png';
 		}
-	}
+	};
 
 	//maps champion names for all teammates in each game
 	//j controls the match index
@@ -155,9 +155,9 @@ angular.module('leagueApp')
 				//so we check to make sure it does to prevent error
 				if (data[j].fellowPlayers) {
 					//checking this makes sure we dont check an index that doesnt exist (if there are less than 9 teammates in a game)
-					if (data[j].fellowPlayers[i] != undefined) {
+					if (data[j].fellowPlayers[i] !== undefined) {
 						//sorts to each team
-						if (data[j].fellowPlayers[i].teamId == data[j].teamId) {
+						if (data[j].fellowPlayers[i].teamId === data[j].teamId) {
 							teamChamps[k] = champSelect(data[j].fellowPlayers[i].championId);
 							k++;
 						}
@@ -173,13 +173,13 @@ angular.module('leagueApp')
 			enemyChamps = []; 
 			teamChamps = [];
 		}
-	}
+	};
 	//assigns color based on win/loss result
 	$scope.teamColor = function(data) {
 		$scope.gameInfo.lose = 0;
 		$scope.gameInfo.win = 0;
 		for (var i = 0; i < 10; i++) {
-			if (data[i].stats.win != true) {
+			if (data[i].stats.win !== true) {
 				teamColor[i] = '#f9bcbc';
 				$scope.gameInfo.lose = $scope.gameInfo.lose + 1;
 			}
@@ -190,7 +190,7 @@ angular.module('leagueApp')
 
 			$scope.gameInfo[i].color = teamColor[i];
 		}
-	}
+	};
 
 	$scope.gameAverages = function(data) {
 		killAverage = 0;
@@ -198,17 +198,17 @@ angular.module('leagueApp')
 		assistAverage = 0;
 		csAverage = 0;
 		for (var i = 0; i < 10; i++) {
-			if (data[i].stats.championsKilled != undefined) {
+			if (data[i].stats.championsKilled !== undefined) {
 				killAverage = killAverage + data[i].stats.championsKilled;
 			}
-			if (data[i].stats.numDeaths != undefined) {
+			if (data[i].stats.numDeaths !== undefined) {
 				deathAverage = deathAverage + data[i].stats.numDeaths;
 			}
-			if (data[i].stats.assists != undefined) {
+			if (data[i].stats.assists !== undefined) {
 				assistAverage = assistAverage + data[i].stats.assists;
 			}
-			if (data[i].stats.minionsKilled != undefined) {
-				if (data[i].stats.neutralMinionsKilled != undefined) {
+			if (data[i].stats.minionsKilled !== undefined) {
+				if (data[i].stats.neutralMinionsKilled !== undefined) {
 					csAverage = csAverage + data[i].stats.minionsKilled + data[i].stats.neutralMinionsKilled;
 				}
 				else {
@@ -220,57 +220,59 @@ angular.module('leagueApp')
 		$scope.gameInfo.deathAverage = deathAverage/10;
 		$scope.gameInfo.assistAverage = assistAverage/10;
 		$scope.gameInfo.csAverage = csAverage/10;
-	}
+	};
 	//goes through each item in each game to get rid of the undefined values and set to 'empty' which provides a 1x1 transparent image
 	$scope.itemFix = function(data) {
+		//need to do this in a not awful way
 		for (var i = 0; i < 10; i++) {
-			if (data[i].stats.item0 == undefined) {
+			if (data[i].stats.item0 === undefined) {
 				data[i].stats.item0 = 'empty';
 			}
-			if (data[i].stats.item1 == undefined) {
+			if (data[i].stats.item1 === undefined) {
 				data[i].stats.item1 = 'empty';
 			}
-			if (data[i].stats.item2 == undefined) {
+			if (data[i].stats.item2 === undefined) {
 				data[i].stats.item2 = 'empty';
 			}
-			if (data[i].stats.item3 == undefined) {
+			if (data[i].stats.item3 === undefined) {
 				data[i].stats.item3 = 'empty';
 			}
-			if (data[i].stats.item4 == undefined) {
+			if (data[i].stats.item4 === undefined) {
 				data[i].stats.item4 = 'empty';
 			}
-			if (data[i].stats.item5 == undefined) {
+			if (data[i].stats.item5 === undefined) {
 				data[i].stats.item5 = 'empty';
 			}
-			if (data[i].stats.item6 == undefined) {
+			if (data[i].stats.item6 === undefined) {
 				data[i].stats.item6 = 'empty';
 			}
-			console.log(data[i].stats.item5)
 		}
-	}
-	$scope.itemHover = function(item) {
-		if (item == 'empty') {
-			return;
-		}
-		$http.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/item/' + item + '?api_key=' + apiKey).
-		success (function(json) {
-			console.log(json);
-			itemInfo = json;
-			$scope.itemInfo = itemInfo;
-		}).
-		error (function() {
-			alert('not ok');
-		})
-	}
-	$scope.itemLeave = function() {
-		// itemInfo = [];
-		// $scope.itemInfo = itemInfo;
-		console.log('OK');
-	}
+	};
+	//not currently in use
+
+	// $scope.itemHover = function(item) {
+	// 	if (item === 'empty') {
+	// 		return;
+	// 	}
+	// 	$http.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/item/' + item + '?api_key=' + apiKey).
+	// 	success (function(json) {
+	// 		console.log(json);
+	// 		itemInfo = json;
+	// 		$scope.itemInfo = itemInfo;
+	// 	}).
+	// 	error (function() {
+	// 		alert('not ok');
+	// 	});
+	// }
+	// $scope.itemLeave = function() {
+	// 	// itemInfo = [];
+	// 	// $scope.itemInfo = itemInfo;
+	// 	console.log('OK');
+	// }
 	//default search!
 	$scope.defaultSearch = function() {
 		$scope.summonerName = 'Dyrus';
 		$scope.apiCall();
-	}
-	$scope.defaultSearch();
+	};
+	// $scope.defaultSearch();
   });
