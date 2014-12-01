ae0b4a2d845e78cd1995ae1bbf08a53771677e23
Justin <zertukis@gmail.com>
2014-12-01 02:50:37 -0600

    changing grunt file for build to not rename images

--- Gruntfile.js
+++ Gruntfile.js
@@ -175,10 +175,6 @@ module.exports = function (grunt) {
     filerev: {
       dist: {
         src: [
-          '<%= yeoman.dist %>/scripts/{,*/}*.js',
-          '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-          '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
     },
